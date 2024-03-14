12795a5eb7adb876687c9cf4b647f6317b80e5e3
nipamrohit <130361623+nipamrohit@users.noreply.github.com>
2024-03-14 20:44:48 +0530

    Update and rename abc to PRACTICAL-1.py

---
+++ Practicals /PRACTICAL-1.py
@@ -0,0 +1,13 @@
+# Practical 1 - Write a python program to calculate simple interest
+# I = Simple Interest
+# P is the principal amount,
+# R is the interest rate (in percentage),
+# T is the time period (in years).
+# For calculate Simple Interest rate R = (100 * I) / (P * T)
+
+P = float(input("Enter Principle Amount : "))
+I = float(input("Enter The Simple Interest : "))
+T = float(input("Enter The time period (in years) : "))
+
+R = (100 * I) / (P * T)
+print("Simple Interest Rate is : ", R, "%") 
